9dff6a5ef4a8ad1c3e985bd12ad7bfe8b6981d4c
TeamSpen210 <spencerb21@live.com>
2018-03-26 13:51:17 +1000

    Improve tkinter.dialog stub

--- stdlib/3/tkinter/dialog.pyi
+++ stdlib/3/tkinter/dialog.pyi
@@ -1,10 +1,20 @@
-from typing import Any, Mapping, Optional
-from tkinter import Widget
+from typing import Any, Dict, Union, Iterable, Mapping, Optional
+from tkinter import Widget, Misc
 
 DIALOG_ICON: str
 
 class Dialog(Widget):
     widgetName: str = ...
     num: int = ...
-    def __init__(self, master: Optional[Any] = ..., cnf: Mapping[str, Any] = ..., **kw) -> None: ...
+    def __init__(
+        self,
+        master: Misc = None,
+        cnf: Mapping[str, Union[str, Iterable[str]] = ..., 
+        *,
+        title: str='',
+        text: str='',
+        bitmap: str='',
+        default: str='',
+        strings: Iterable[str]=(),
+    ) -> None: ...
     def destroy(self) -> None: ...
