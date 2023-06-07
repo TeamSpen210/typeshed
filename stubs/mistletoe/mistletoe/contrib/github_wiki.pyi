8736e30ad87ee4b7cb50164d8fb56aa0d9143782
Spencer Brown <spencerb21@live.com>
2023-06-07 16:49:01 +1000

    Type mistletoe.contrib.github_wiki

--- stubs/mistletoe/mistletoe/contrib/github_wiki.pyi
+++ stubs/mistletoe/mistletoe/contrib/github_wiki.pyi
@@ -1,13 +1,15 @@
-from _typeshed import Incomplete
+import re
+from typing import ClassVar
 
+from mistletoe.core_tokens import MatchObj
 from mistletoe.html_renderer import HTMLRenderer
 from mistletoe.span_token import SpanToken
 
 class GithubWiki(SpanToken):
-    pattern: Incomplete
-    target: Incomplete
-    def __init__(self, match) -> None: ...
+    pattern: ClassVar[re.Pattern[str]]
+    target: str
+    def __init__(self, match: MatchObj) -> None: ...
 
 class GithubWikiRenderer(HTMLRenderer):
     def __init__(self) -> None: ...
-    def render_github_wiki(self, token): ...
+    def render_github_wiki(self, token: GithubWiki) -> str: ...
