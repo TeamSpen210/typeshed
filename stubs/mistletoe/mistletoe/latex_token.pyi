729c1b3b67274cb8172622e0bda89b9db3844440
Spencer Brown <spencerb21@live.com>
2023-06-07 16:43:06 +1000

    Type mistletoe.latex_token and _renderer

--- stubs/mistletoe/mistletoe/latex_token.pyi
+++ stubs/mistletoe/mistletoe/latex_token.pyi
@@ -1,8 +1,9 @@
-from _typeshed import Incomplete
+import re
+from typing import ClassVar
 
 import mistletoe.span_token as span_token
 
 class Math(span_token.SpanToken):
-    pattern: Incomplete
-    parse_inner: bool
-    parse_group: int
+    pattern: ClassVar[re.Pattern[str]]
+    parse_inner: ClassVar[bool] = False
+    parse_group: ClassVar[int] = 0
